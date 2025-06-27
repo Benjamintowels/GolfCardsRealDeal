bd4dbd8fd2f61f0b33b2b5c6de6fa53f65a5a825
RadiantUnderground <towels2110@gmail.com>
2025-06-27 14:06:10 -0600

    MidGameShop

--- Main.gd
+++ Main.gd
@@ -5,6 +5,7 @@ extends Control
 @onready var character3_button = $UI/Character3Button
 @onready var start_round_button = $UI/StartRoundButton
 @onready var start_putt_putt_button = $UI/StartPuttPutt
+@onready var start_back_9_button = $UI/StartBack9
 
 var selected_character = 1  # Default to character 1
 
@@ -24,20 +25,22 @@ func _ready():
 	character3_button.pressed.connect(_on_character3_selected)
 	start_round_button.pressed.connect(_on_start_round_pressed)
 	start_putt_putt_button.pressed.connect(_on_start_putt_putt_button_pressed)
+	start_back_9_button.pressed.connect(_on_start_back_9_pressed)
 	
 	print("Buttons connected successfully")
+	print("Initial selected_character: ", selected_character)
 
 func _on_character1_selected():
 	selected_character = 1
-	print("Character 1 selected")
+	print("Character 1 (Layla) selected, selected_character = ", selected_character)
 
 func _on_character2_selected():
 	selected_character = 2
-	print("Character 2 selected")
+	print("Character 2 (Benny) selected, selected_character = ", selected_character)
 
 func _on_character3_selected():
 	selected_character = 3
-	print("Character 3 selected")
+	print("Character 3 (Clark) selected, selected_character = ", selected_character)
 
 func _on_start_round_pressed():
 	# Store the selected character in a global variable
@@ -57,6 +60,19 @@ func _on_start_putt_putt_button_pressed():
 	# Change scene on next frame
 	call_deferred("_change_scene")
 
+func _on_start_back_9_pressed():
+	# Store the selected character in a global variable
+	Global.selected_character = selected_character
+	print("Selected character: ", selected_character, " - Starting Back 9")
+	print("Global.selected_character set to: ", Global.selected_character)
+	
+	# Change scene to MidGameShop
+	call_deferred("_change_to_mid_game_shop")
+
 func _change_scene():
 	# Use FadeManager for smooth transition
 	FadeManager.fade_to_black(func(): get_tree().change_scene_to_file("res://Course1.tscn"), 0.5)
+
+func _change_to_mid_game_shop():
+	# Use FadeManager for smooth transition to MidGameShop
+	FadeManager.fade_to_black(func(): get_tree().change_scene_to_file("res://MidGameShop.tscn"), 0.5)
---
+++ mid_game_shop.gd
@@ -0,0 +1,45 @@
+extends Control
+
+@onready var character_image: TextureRect = $CharacterImage
+@onready var start_back_9_button: Button = $StartBack9Button
+
+func _ready():
+	# Connect the button signal
+	start_back_9_button.pressed.connect(_on_start_back_9_pressed)
+	
+	# Set initial character image based on selected character
+	_set_character_image()
+
+func _set_character_image():
+	# Get the selected character from Global
+	var selected_character = Global.selected_character
+	print("MidGameShop: Global.selected_character = ", selected_character)
+	
+	# Load and set the character image
+	var character_texture = _get_character_texture(selected_character)
+	if character_texture:
+		character_image.texture = character_texture
+		print("MidGameShop: Set character texture for character ", selected_character)
+	else:
+		print("MidGameShop: Failed to load character texture for character ", selected_character)
+
+func _get_character_texture(character_number: int) -> Texture2D:
+	print("MidGameShop: Getting texture for character number: ", character_number)
+	
+	# Map character numbers to the specific mid-game character textures
+	var character_textures = {
+		1: preload("res://LaylaMid.png"),    # Layla
+		2: preload("res://BennyMid.png"),    # Benny  
+		3: preload("res://ClarkMid.png"),    # Clark
+	}
+	
+	var texture = character_textures.get(character_number, character_textures[1])
+	print("MidGameShop: Returning texture for character ", character_number)
+	return texture
+
+func _on_start_back_9_pressed():
+	# Handle starting the back 9 holes
+	print("Starting Back 9...")
+	# Add your logic here to transition to the back 9 holes
+	# For example:
+	# get_tree().change_scene_to_file("res://path_to_back_9_scene.tscn")
